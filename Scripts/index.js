6306216cec7635ece30b9260c8e2875ea46e67df
rbrown95 <77420089+rbrown95@users.noreply.github.com>
2021-04-09 15:47:14 -0400

    Add files via upload

--- Scripts/index.js
+++ Scripts/index.js
@@ -10,18 +10,9 @@ function getWeather() {
         console.log(position);
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
-        var image = document.getElementById("weatherImage");
     
-        let url =
-          api +
-          "?lat=" +
-          latitude +
-          "&lon=" +
-          longitude +
-          "&appid=" +
-          apiKey +
-          "&units=imperial";
-    
+        let url = api + "?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=imperial";
+
           fetch(url)
           .then(response => response.json())
           .then(data => {
@@ -30,16 +21,43 @@ function getWeather() {
             temperature.innerHTML = temp + "° F";
             location.innerHTML = data.name;
             description.innerHTML = data.weather[0].main;
-            
-            if (data.weather[0].main === 'Clouds') {
-                image.setAttribute('src', 'Images/cloudy.svg');
-            }
           });
     }
        
     function error() {
         console.log("error");
     }
+
+}
+
+
+function search() {
+    var temperature = document.getElementById("temperature");
+    var description = document.getElementById("description");
+    var location = document.getElementById("location");
+    let api = "https://api.openweathermap.org/data/2.5/weather";
+    let apiKey = "86abcac431bd1f434ea1e29bd6903bfe";
+    var city = document.getElementById("search").value;
+    let url = "default"
+
+    console.log(typeof city);
+
+    url = api + "?q=" + city + "&appid=" + apiKey + "&units=imperial";
+
+    temperature.innerHTML = "";
+    description.innerHTML = "";
+    location.innerHTML = "";
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        let temp = data.main.temp;
+        temperature.innerHTML = temp + "° F";
+        location.innerHTML = data.name;
+        description.innerHTML = data.weather[0].main;
+        
+      });
 }
 
 
